3b4a9e7d068acec236e84891f50fe4862eb52186
Vid Mio <vidmio@gmail.com>
2024-02-13 13:19:06 +0100

    add Generic

---
+++ GenericRadzenComplexModelBlazor/Components/Repository/GenericRepository.cs
@@ -0,0 +1,73 @@
+﻿using GenericRadzenComplexModelBlazor.Components.Repository.Interface;
+using GenericRadzenComplexModelBlazor.Components.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace GenericRadzenComplexModelBlazor.Components.Repository
+{
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class
+    {
+        private readonly IDbContextFactory<ApplicationDbContext> _context;
+
+        public GenericRepository(IDbContextFactory<ApplicationDbContext> context)
+        {
+            _context = context;
+        }
+
+        public virtual async Task<List<TEntity>> Get()
+        {
+            using var db = await _context.CreateDbContextAsync();
+            return await Task.Run(() => db.Set<TEntity>().ToListAsync());
+        }
+
+        public async Task<TEntity?> Get(int id)
+        {
+            using var db = await _context.CreateDbContextAsync();
+            return await db.Set<TEntity>().FindAsync(id);
+        }
+
+        public bool Add(in TEntity sender)
+        {
+            using var db = _context.CreateDbContext();
+            db.Entry(sender).State = EntityState.Added;
+            try
+            {
+                db.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool Update(in TEntity sender)
+        {
+            using var db = _context.CreateDbContext();
+            db.Entry(sender).State = EntityState.Modified;
+            try
+            {
+                db.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool Delete(in TEntity sender)
+        {
+            using var db = _context.CreateDbContext();
+            try
+            {
+                db.Remove(sender);
+                db.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
